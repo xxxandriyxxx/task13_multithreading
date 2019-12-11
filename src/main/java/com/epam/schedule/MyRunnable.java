564d6b27feb2eec5d1846d2ccc70086189b353a5
Andriy <andriymattt@gmail.com>
2019-12-12 01:51:19 +0200

    feature/schedule: add MyRunnable class

---
+++ src/main/java/com/epam/schedule/MyRunnable.java
@@ -0,0 +1,17 @@
+package com.epam.schedule;
+
+import java.time.LocalTime;
+
+public class MyRunnable implements Runnable {
+
+    private int taskNumber;
+
+    public MyRunnable(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task " + taskNumber + " ---> time of starting:  " + LocalTime.now());
+    }
+}
