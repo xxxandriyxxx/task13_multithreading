e22358c4db45690db44c0e23be37b1ba06a87147
Andriy <andriymattt@gmail.com>
2019-12-11 23:50:13 +0200

    feature/fibonacci: add runFibonacciByThread method to Application class

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,12 +1,35 @@
 package com.epam;
 
+import com.epam.fibonacci.FibonacciRunnable;
 import com.epam.ping_pong.PingPong;
 
 public class Application {
 
     public static void main(String[] args) {
-        PingPong pingPong = new PingPong();
-        pingPong.show(10000);
+
+        playPingPong(10000);
+        runFibonacciByThread(10);
     }
-    
+
+
+    private static void playPingPong(int timesNumber) {
+        PingPong pingPong = new PingPong();
+        pingPong.show(timesNumber);
+    }
+
+    private static void runFibonacciByThread(int size) {
+        System.out.println("Fibonacci sequence of " + size + " numbers:");
+        try {
+            for (int i = 0; i < size; i++) {
+                FibonacciRunnable f = new FibonacciRunnable(i);
+                Thread thread = new Thread(f);
+                thread.start();
+                thread.join();
+                System.out.println("num" + (i + 1) + " = " + f.number);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
